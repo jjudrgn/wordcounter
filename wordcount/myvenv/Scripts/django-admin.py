e20884f8782d051678216e00f85c759654d75422
jjudrgn <wndyd9557@gmail.com>
2019-04-30 17:51:55 +0900

    first commit of wordcount proj

---
+++ wordcount/myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\default.desktop-dshs62s\desktop\web\likelion\wordcount\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
